87d3a2136d4567db0900ad8e57c51ba47a074321
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-08-23 22:20:12 +0600

    feat: graph component

--- src/components/dashboard/aoi-graphs/aoi-graphs.tsx
+++ src/components/dashboard/aoi-graphs/aoi-graphs.tsx
@@ -1,8 +1,37 @@
-function AreaGraphs({ title }: { title: string }) {
-  console.log("title", title);
+import { useSelector } from "react-redux";
+import { RootState } from "../../../app/store";
+import SlopeValue from "../aoi-graphs/slopevalue/slopevalue";
+
+function AreaGraphs() {
+  const link = useSelector((state: RootState) => state.area.selectedLink);
+  console.log(typeof link);
   return (
     <>
-      <h3>{title}</h3>
+      {(() => {
+        if (link === "Ascending")
+          return (
+            <>
+              <SlopeValue
+                title={link}
+                maxUp={56}
+                maxDown={-193}
+                average={-52}
+              />
+            </>
+          );
+
+        if (link === "Descending")
+          return (
+            <>
+              <SlopeValue
+                title={link}
+                maxUp={60}
+                maxDown={-193}
+                average={-52}
+              />
+            </>
+          );
+      })()}
     </>
   );
 }
