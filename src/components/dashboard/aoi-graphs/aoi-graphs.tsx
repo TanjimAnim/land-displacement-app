34749ab3577b2c008f103a3d34b931651bdf0733
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-08-28 14:13:27 +0600

    feat:title style added,logic if no link selected

--- src/components/dashboard/aoi-graphs/aoi-graphs.tsx
+++ src/components/dashboard/aoi-graphs/aoi-graphs.tsx
@@ -1,54 +1,72 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../../app/store";
 import SlopeValue from "../aoi-graphs/slopevalue/slopevalue";
-import Overview from "./slopevalue/overview";
+import Overview from "./slopevalue/overview/overview";
+import styled from "styled-components";
+
+const Title = styled.div`
+  font-weight: 400;
+  font-size: 24px;
+  color: white;
+  margin-top: 40px;
+  margin-left: 40px;
+`;
 
 function AreaGraphs() {
   const link = useSelector((state: RootState) => state.area.selectedLink);
-  console.log(link);
+  const area = useSelector((state: RootState) => state.area);
   return (
     <>
-      {(() => {
-        if (link === "Overview")
-          return (
-            <>
-              <Overview />
-            </>
-          );
-        if (link === "Ascending")
-          return (
-            <>
-              <SlopeValue
-                title={link}
-                maxUp={56}
-                maxDown={-193}
-                average={-52}
-              />
-            </>
-          );
+      {Object.keys(area.value).length === 0 ? (
+        <>
+          <Title>Select an AOI to display data</Title>
+        </>
+      ) : (
+        <>
+          {(() => {
+            if (link === "") return <></>;
+            if (link === "Overview")
+              return (
+                <>
+                  <Overview />
+                </>
+              );
+            if (link === "Ascending")
+              return (
+                <>
+                  <SlopeValue
+                    title={link}
+                    maxUp={56}
+                    maxDown={-193}
+                    average={-52}
+                  />
+                </>
+              );
 
-        if (link === "Descending")
-          return (
-            <>
-              <SlopeValue
-                title={link}
-                maxUp={60}
-                maxDown={-193}
-                average={-52}
-              />
-            </>
-          );
-        if (link === "Vertical" || "Horizontal")
-          return (
-            <>
-              <SlopeValue
-                title={link}
-                avgTotalDisplacement={-12}
-                displacementRate={-52}
-              />
-            </>
-          );
-      })()}
+            if (link === "Descending")
+              return (
+                <>
+                  <SlopeValue
+                    title={link}
+                    maxUp={60}
+                    maxDown={-193}
+                    average={-52}
+                  />
+                </>
+              );
+            if (link === "Vertical" || "Horizontal")
+              return (
+                <>
+                  <SlopeValue
+                    title={link}
+                    avgTotalDisplacement={-12}
+                    displacementRate={-52}
+                  />
+                </>
+              );
+          })()}
+        </>
+      )}
     </>
   );
 }
