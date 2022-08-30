4cffa8645beccc8742b7bcec5706720c31302885
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-08-30 21:48:59 +0600

    feat: added style,usedispatch

--- src/components/profile/notification.tsx
+++ src/components/profile/notification.tsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import { removeNotification } from "../../features/slice";
 import type { RootState } from "../../app/store";
 import styled from "styled-components";
 import { InfoCircleFill } from "@styled-icons/bootstrap/InfoCircleFill";
@@ -6,6 +7,7 @@ import { ErrorCircle } from "@styled-icons/fluentui-system-filled/ErrorCircle";
 import { ErrorWarning } from "@styled-icons/remix-fill/ErrorWarning";
 import { CircleCheck } from "@styled-icons/open-iconic/CircleCheck";
 import { CloseOutline } from "@styled-icons/evaicons-outline/CloseOutline";
+import { ThemeProvider } from "styled-components";
 
 const Wrapper = styled.div`
   width: 309px;
@@ -25,6 +27,12 @@ const NotificationMessage = styled.div`
   justify-content: space-between;
   align-items: flex-start;
   padding: 1rem;
+  border-left: ${(props) => {
+    if (props.theme.status === "info") return "3px solid #0f62fe";
+    if (props.theme.status === "success") return "3px solid #24A148";
+    if (props.theme.status === "error") return "3px solid #DA1E28";
+    if (props.theme.status === "warning") return "3px solid #F1C21B";
+  }};
 `;
 
 const StatusTitle = styled.div`
@@ -47,6 +55,7 @@ const Container = styled.div`
 `;
 
 function NotificationWindow() {
+  const dispatch = useDispatch();
   const notification: object[] = useSelector(
     (state: RootState) => state.area.notification
   );
@@ -56,165 +65,208 @@ function NotificationWindow() {
 
   return (
     <>
-      <Wrapper>
-        <NotificationDayBar>
-          <div>Today</div>
-          <div style={{ color: "#78A9FF" }}>Mark all as read</div>
-        </NotificationDayBar>
-        {notification.map((item: any) => {
-          if (item.currentDay === day) {
-            return (
-              <>
-                <NotificationMessage id={item.id}>
-                  <Container>
-                    {(() => {
-                      if (item.status === "info") {
-                        return <InfoCircleFill color='#0f62fe' width='16px' />;
-                      }
-                      if (item.status === "success") {
-                        return <CircleCheck color='#24A148' width='16px' />;
-                      }
-                      if (item.status === "error") {
-                        return <ErrorCircle color='#DA1E28' width='16px' />;
-                      }
-                      if (item.status === "warning") {
-                        return <ErrorWarning color='#F1C21B' width='16px' />;
-                      }
-                    })()}
-                  </Container>
-                  <Container>
-                    <StatusTitle>
-                      {(() => {
-                        if (item.status === "info") {
-                          return "Informational Notification";
-                        }
-                        if (item.status === "success") {
-                          return "Success Notification";
-                        }
-                        if (item.status === "error") {
-                          return "Error Notification";
-                        }
-                        if (item.status === "warning") {
-                          return "Warning Notification";
-                        }
-                      })()}
-                    </StatusTitle>
-                    <Message>{item.message}</Message>
-                    <DateString>{item.dateString}</DateString>
-                  </Container>
-                  <div>
-                    <CloseOutline width='16px' color='black' />
-                  </div>
-                </NotificationMessage>
-              </>
-            );
-          }
-        })}
-        <NotificationDayBar>
-          <div>Yesterday</div>
-        </NotificationDayBar>
-        {notification.map((item: any) => {
-          if (item.currentDay === day - 1) {
-            return (
-              <>
-                <NotificationMessage id={item.id}>
-                  <Container>
-                    {(() => {
-                      if (item.status === "info") {
-                        return <InfoCircleFill color='#0f62fe' width='16px' />;
-                      }
-                      if (item.status === "success") {
-                        return <CircleCheck color='#24A148' width='16px' />;
-                      }
-                      if (item.status === "error") {
-                        return <ErrorCircle color='#DA1E28' width='16px' />;
-                      }
-                      if (item.status === "warning") {
-                        return <ErrorWarning color='#F1C21B' width='16px' />;
-                      }
-                    })()}
-                  </Container>
-                  <Container>
-                    <StatusTitle>
-                      {(() => {
-                        if (item.status === "info") {
-                          return "Informational Notification";
-                        }
-                        if (item.status === "success") {
-                          return "Success Notification";
-                        }
-                        if (item.status === "error") {
-                          return "Error Notification";
-                        }
-                        if (item.status === "warning") {
-                          return "Warning Notification";
-                        }
-                      })()}
-                    </StatusTitle>
-                    <Message>{item.message}</Message>
-                    <DateString> {item.dateString}</DateString>
-                  </Container>
-                  <div>
-                    <CloseOutline width='16px' color='black' />
-                  </div>
-                </NotificationMessage>
-              </>
-            );
-          }
-        })}
-        <NotificationDayBar>
-          <div>Older</div>
-        </NotificationDayBar>
-        {notification.map((item: any) => {
-          if (item.currentDay === day - 2) {
-            return (
-              <>
-                <NotificationMessage id={item.id}>
-                  <Container>
-                    {(() => {
-                      if (item.status === "info") {
-                        return <InfoCircleFill color='#0f62fe' width='16px' />;
-                      }
-                      if (item.status === "success") {
-                        return <CircleCheck color='#24A148' width='16px' />;
-                      }
-                      if (item.status === "error") {
-                        return <ErrorCircle color='#DA1E28' width='16px' />;
-                      }
-                      if (item.status === "warning") {
-                        return <ErrorWarning color='#F1C21B' width='16px' />;
-                      }
-                    })()}
-                  </Container>
-                  <Container>
-                    <StatusTitle>
-                      {(() => {
-                        if (item.status === "info") {
-                          return "Informational Notification";
-                        }
-                        if (item.status === "success") {
-                          return "Success Notification";
-                        }
-                        if (item.status === "error") {
-                          return "Error Notification";
-                        }
-                        if (item.status === "warning") {
-                          return "Warning Notification";
-                        }
-                      })()}
-                    </StatusTitle>
-                    <Message>{item.message}</Message>
-                    <DateString>{item.dateString}</DateString>
-                  </Container>
-                  <div>
-                    <CloseOutline width='16px' color='black' />
-                  </div>
-                </NotificationMessage>
-              </>
-            );
-          }
-        })}
-      </Wrapper>
+      {notification.length === 0 ? (
+        <Wrapper>
+          <NotificationDayBar>No New Notification</NotificationDayBar>
+        </Wrapper>
+      ) : (
+        <Wrapper>
+          <NotificationDayBar>
+            <div>Today</div>
+            <div style={{ color: "#78A9FF" }}>Mark all as read</div>
+          </NotificationDayBar>
+          {notification.map((item: any) => {
+            if (item.currentDay === day) {
+              return (
+                <>
+                  <ThemeProvider theme={item}>
+                    <NotificationMessage key={item.id}>
+                      <Container>
+                        {(() => {
+                          if (item.status === "info") {
+                            return (
+                              <InfoCircleFill color='#0f62fe' width='16px' />
+                            );
+                          }
+                          if (item.status === "success") {
+                            return <CircleCheck color='#24A148' width='16px' />;
+                          }
+                          if (item.status === "error") {
+                            return <ErrorCircle color='#DA1E28' width='16px' />;
+                          }
+                          if (item.status === "warning") {
+                            return (
+                              <ErrorWarning color='#F1C21B' width='16px' />
+                            );
+                          }
+                        })()}
+                      </Container>
+                      <Container>
+                        <StatusTitle>
+                          {(() => {
+                            if (item.status === "info") {
+                              return "Informational Notification";
+                            }
+                            if (item.status === "success") {
+                              return "Success Notification";
+                            }
+                            if (item.status === "error") {
+                              return "Error Notification";
+                            }
+                            if (item.status === "warning") {
+                              return "Warning Notification";
+                            }
+                          })()}
+                        </StatusTitle>
+                        <Message>{item.message}</Message>
+                        <DateString>{item.dateString}</DateString>
+                      </Container>
+                      <div>
+                        <CloseOutline
+                          width='16px'
+                          color='black'
+                          onClick={() => {
+                            dispatch(removeNotification(item.id));
+                          }}
+                        />
+                      </div>
+                    </NotificationMessage>
+                  </ThemeProvider>
+                </>
+              );
+            }
+          })}
+          <NotificationDayBar>
+            <div>Yesterday</div>
+          </NotificationDayBar>
+          {notification.map((item: any) => {
+            if (item.currentDay === day - 1) {
+              return (
+                <>
+                  <ThemeProvider theme={item}>
+                    <NotificationMessage key={item.id}>
+                      <Container>
+                        {(() => {
+                          if (item.status === "info") {
+                            return (
+                              <InfoCircleFill color='#0f62fe' width='16px' />
+                            );
+                          }
+                          if (item.status === "success") {
+                            return <CircleCheck color='#24A148' width='16px' />;
+                          }
+                          if (item.status === "error") {
+                            return <ErrorCircle color='#DA1E28' width='16px' />;
+                          }
+                          if (item.status === "warning") {
+                            return (
+                              <ErrorWarning color='#F1C21B' width='16px' />
+                            );
+                          }
+                        })()}
+                      </Container>
+                      <Container>
+                        <StatusTitle>
+                          {(() => {
+                            if (item.status === "info") {
+                              return "Informational Notification";
+                            }
+                            if (item.status === "success") {
+                              return "Success Notification";
+                            }
+                            if (item.status === "error") {
+                              return "Error Notification";
+                            }
+                            if (item.status === "warning") {
+                              return "Warning Notification";
+                            }
+                          })()}
+                        </StatusTitle>
+                        <Message>{item.message}</Message>
+                        <DateString> {item.dateString}</DateString>
+                      </Container>
+                      <div>
+                        <CloseOutline
+                          width='16px'
+                          color='black'
+                          onClick={() => {
+                            dispatch(removeNotification(item.id));
+                          }}
+                        />
+                      </div>
+                    </NotificationMessage>
+                  </ThemeProvider>
+                </>
+              );
+            }
+          })}
+          <NotificationDayBar>
+            <div>Older</div>
+          </NotificationDayBar>
+          {notification.map((item: any) => {
+            if (item.currentDay === day - 2) {
+              return (
+                <>
+                  <ThemeProvider theme={item}>
+                    <NotificationMessage key={item.id}>
+                      <Container>
+                        {(() => {
+                          if (item.status === "info") {
+                            return (
+                              <InfoCircleFill color='#0f62fe' width='16px' />
+                            );
+                          }
+                          if (item.status === "success") {
+                            return <CircleCheck color='#24A148' width='16px' />;
+                          }
+                          if (item.status === "error") {
+                            return <ErrorCircle color='#DA1E28' width='16px' />;
+                          }
+                          if (item.status === "warning") {
+                            return (
+                              <ErrorWarning color='#F1C21B' width='16px' />
+                            );
+                          }
+                        })()}
+                      </Container>
+                      <Container>
+                        <StatusTitle>
+                          {(() => {
+                            if (item.status === "info") {
+                              return "Informational Notification";
+                            }
+                            if (item.status === "success") {
+                              return "Success Notification";
+                            }
+                            if (item.status === "error") {
+                              return "Error Notification";
+                            }
+                            if (item.status === "warning") {
+                              return "Warning Notification";
+                            }
+                          })()}
+                        </StatusTitle>
+                        <Message>{item.message}</Message>
+                        <DateString>{item.dateString}</DateString>
+                      </Container>
+                      <div>
+                        <CloseOutline
+                          width='16px'
+                          color='black'
+                          onClick={() => {
+                            dispatch(removeNotification(item.id));
+                          }}
+                        />
+                      </div>
+                    </NotificationMessage>
+                  </ThemeProvider>
+                </>
+              );
+            }
+          })}
+          <NotificationDayBar />
+        </Wrapper>
+      )}
     </>
   );
 }
