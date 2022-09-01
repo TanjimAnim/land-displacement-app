90a932bde8272c22b03b7b78d728dfe5c9251f79
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-09-01 14:29:36 +0600

    feat:display map component

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,7 +1,29 @@
+import MapDisplay from "./mapDisplay";
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import styled from "styled-components";
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  height: 100vh;
+`;
+
 function Map() {
+  const value = useSelector((state: RootState) => state.area.value);
+
   return (
     <>
-      <h1>Map</h1>
+      {value.length === 0 ? (
+        <>
+          <Title>Select an AOI to display Map</Title>
+        </>
+      ) : (
+        <MapDisplay />
+      )}
     </>
   );
 }
