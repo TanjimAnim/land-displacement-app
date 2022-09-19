a891b9d0391428f602f44cc4c26e9ed8fc848832
TanjimAnim <53538004+tanjimanim555@users.noreply.github.com>
2022-09-19 13:29:00 +0600

    fix:reordering of imports

--- src/components/map/mapDisplay.tsx
+++ src/components/map/mapDisplay.tsx
@@ -3,15 +3,15 @@ import * as mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./mapDisplay.css";
 
-// @ts-ignore
-// eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass =
-  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
-
 import { useSelector } from "react-redux";
 import type { RootState } from "../../app/store";
 import styled from "styled-components";
 import { useRef, useEffect, useState } from "react";
+
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass =
+  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 const Title = styled.div`
   display: flex;
